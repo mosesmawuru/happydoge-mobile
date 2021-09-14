a3d9c2d13311256884a37e1176b408bcf5d64f67
stoneheart428 <lovely4you2ai8ni@gmail.com>
2021-09-14 18:10:36 +0900

    feat:add notification

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,47 @@
-import React from 'react';
-import Routes from './src/pages';
-import Web3 from 'web3';
+import React, {Component} from 'react';
+import {TextInput, Text, View, TouchableOpacity, Alert} from 'react-native';
+import NotifService from './src/notification/NotifService';
 
-const App = () => {
-  return <Routes />;
-};
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.notif = new NotifService(
+      this.onRegister.bind(this),
+      this.onNotif.bind(this),
+    );
+  }
+
+  render() {
+    return (
+      <View>
+        <View />
+        <TextInput
+          value={this.state.registerToken}
+          placeholder="Register token"
+        />
+        <View />
+
+        <TouchableOpacity
+          onPress={() => {
+            this.notif.localNotif();
+          }}>
+          <Text>Local Notification (now)</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  onRegister(token) {
+    this.setState({registerToken: token.token, fcmRegistered: true});
+  }
+
+  onNotif(notif) {
+    Alert.alert(notif.title, notif.message);
+  }
+
+  handlePerm(perms) {
+    Alert.alert('Permissions', JSON.stringify(perms));
+  }
+}
